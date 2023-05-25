1d6e632a8faf4a4a49d563309a5b26dfd4bae6b7
jbucsa <133785969+jbucsa@users.noreply.github.com>
2023-05-25 10:27:57 -0500

    Model 5 JS Storing Variables Activity.js

---
+++ .js
@@ -0,0 +1,13 @@
+var restaurantName = "The Chef in the Hat";
+var phoneNumber = 8723498066;
+var locationCity = "Chicago";
+var familyFriendly = true;
+var priceRange = "$$";
+
+console.log(
+  typeof restaurantName,
+  typeof phoneNumber,
+  typeof city,
+  typeof familyFriendly,
+  typeof priceRange
+);
